98edd43ffc0f43722822c3510b5a07c84454e1e2
ZyneAA <htet.zyne.aa@gmail.com>
2026-04-23 13:27:08 +0630

    empirical risk penalty impl in py

--- math_python/empirical_risk_minimization.py
+++ math_python/empirical_risk_minimization.py
@@ -6,6 +6,61 @@ app = marimo.App(width="medium")
 
 @app.cell
 def _():
+    import marimo as mo
+
+    return (mo,)
+
+
+@app.cell
+def _():
+    import numpy as np
+    import pandas as pd
+
+    return (np,)
+
+
+@app.cell
+def _(np):
+    labels = np.array(["cat", "dog", "bird", "fish"])
+    return (labels,)
+
+
+@app.cell
+def _(labels, np):
+    random_labels_actual = np.random.choice(labels, size=1000)
+    return (random_labels_actual,)
+
+
+@app.cell
+def _(random_labels_actual):
+    random_labels_actual[:3]
+    return
+
+
+@app.cell
+def _(labels, np):
+    random_labels_predicted = np.random.choice(labels, size=1000)
+    return (random_labels_predicted,)
+
+
+@app.cell
+def _(random_labels_predicted):
+    random_labels_predicted[:3]
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    $L(θ)\ =\ \frac{1}{N} \sum_{n=1}^{N}∏(y_n\ \neq f(x_n; θ))$
+    """)
+    return
+
+
+@app.cell
+def _(np, random_labels_actual, random_labels_predicted):
+    empirical_risk = np.mean(random_labels_actual != random_labels_predicted)
+    empirical_risk
     return
 
 
